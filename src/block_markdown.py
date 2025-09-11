592eaf85810a31c2a1ca67b439d151bd9f2fa4c5
clintonlough <clinton.lough@gmail.com>
2025-09-11 17:32:01 +1000

    consolidated functions into block_markdown file

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,4 +1,5 @@
 from textnode import TextType, TextNode
+from htmlnode import HTMLNode
 import re
 from enum import Enum
 import textwrap
@@ -51,7 +52,59 @@ def block_to_block_type(block):
         return BlockType.ORDERED_LIST
     return BlockType.PARAGRAPH
 
+#Takes a block and converts it into the correct HTML Node
+def block_to_html_node(block, block_type):
+    match block_type:
+        case BlockType.HEADING:
+            print("this is a heading")
+            node = HTMLNode(tag = "h", value = block, children = None, props = None)
+        case BlockType.CODE:
+            pass
+        case BlockType.QUOTE:
+            print("this is a quote")
+        case BlockType.UNORDERED_LIST:
+            pass
+        case BlockType.ORDERED_LIST:
+            print("this is an ordered list")
+        case BlockType.PARAGRAPH:
+            print("this is a paragraph")
+    return
+
+
+def markdown_to_html_node(markdown):
+    #split markdown into blocks
+    blocks = markdown_to_blocks(markdown)
+    # loop over each block
+    for block in blocks:
+        #determine block type
+        block_type = block_to_block_type(block)
+        #create HTMLNode
+        html_node = block_to_html_node(block, block_type)
+        #Assign children HTMLNodes to the block - use a helper
+    
+    #make all the block nodes children under a single parent HTML Node (Div) and return it
+    return
+
+
 def main():
-    pass
+    markdown = """
+###This is the heading
+
+
+1. this is an order
+2. list
+3. of 3 items
+
+
+> and this is a quote
+> about cows
+
+
+This is just a random paragraph
+
+
+"""
+
+    markdown_to_html_node(markdown)
 
 main()
